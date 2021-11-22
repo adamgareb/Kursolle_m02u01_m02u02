324e395d4a752241bc3be6a99dc096d5d2ebe4a5
adamgareb <92087000+adamgareb@users.noreply.github.com>
2021-11-22 10:03:47 +0100

    Add files via upload

---
+++ Adam Hama Gareb - m02u01 och m02u02/m02u01.py
@@ -0,0 +1,15 @@
+#Uppgift m02u01 Adam Hama Gareb TE20D  Datum klar: 2021-11-15
+
+mitt_namn = "Adam Hama Gareb"
+
+print("Jag heter " + mitt_namn + " och jag läser programmering på Alströmergymnasiet.")
+
+#Jag använder mig av enkelfnuttar för hela texten så att jag senare kan lägga till dubbelfnuttar så som jag har lagt nedan
+print('Jag tycker att det verkar "kul" med programmering.')
+
+#Jag använder mig av \n för att göra en ny rad för den nya texten
+print("Dessa tre rader \nhar jag skrivit med \nen enda print()")
+
+#Jag använder mig av \t för att pusha fram punktlistan
+print("Följande saker har jag kommenterat i min kod: \n\t* Hur jag skrev ut dubbelfnuttarna på andra raden \n\t* Hur jag kunde skriva ut tre rader med en print().  \n\t* Hur jag använde mig av tab, för att göra denna listan.")
+
---
+++ Adam Hama Gareb - m02u01 och m02u02/m02u02.py
@@ -0,0 +1,46 @@
+#1.
+a = 9
+b = 2
+f= 4.0
+namn = "Adam"
+
+#2.
+print(a*b)
+print(type(a*b))
+
+#3.
+print(a*f)
+print(type(a*f))
+
+#4. Jag tror att datatypen blir float då svaret kommer bli en decimaltal
+print(a/b)
+print(type(a/b))
+
+#5.
+print(namn)
+print(type(namn))
+
+#6.
+print(a//b)
+print(type(a//b))
+
+#7.
+print(a%b)
+print(type(a%b))
+
+#9. Ett tall upphöjt med 0.5 är samma sak som att räkna roten ur ett tal, som då ger fram roten ur a. Datatypen blir int för att det är ett heltal
+print(a ** 0.5)
+print(type(a ** 0.5))
+
+#10. Ett tal i kvadrat är ett tal upphöjt till 2, och det skriver man så som jag gjorde nedan. Datatypen blir då också int för att det är ett heltal
+print(b ** 2)
+print(type(b ** 2))
+
+#11. Samma som ovan. Datatypen blir då till float då f är 4.0, vilket är ett decimaltal
+print(f ** 2)
+print(type(f ** 2))
+
+#12. Kolla ovan
+
+#13.
+print(a+b)
